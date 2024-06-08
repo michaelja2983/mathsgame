87a2a75b1ea124a26633d39a60cadd798aebac74
Mike Anning <mike@michaelja.co.uk>
2024-06-08 12:54:24 +0000

    add structure to game

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function runGame() {
+
+
+}
+
+function checkAnswer() {
+
+
+}
+
+function calculateCorrectAnswer () {
+
+
+}
+
+function incrementScore () {
+
+
+}
+
+function incrementWrontAnswer() {
+
+
+}
+
+function displayAdditionQuestion() {
+
+
+}
+
+function displaySubtractQuestion() {
+
+
+}
+
+function displayMultiplyQuestion() {
+
+    
+}
